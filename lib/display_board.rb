cc2488f6eca2ac4b9d951fd1c0bdc8bc0d4e2aaf
Jordan Harris <jharris@wework.com>
2018-06-11 16:21:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  x = "   |   |   \n-----------\n   |   |   \n-----------\n   |   | \n"
+  x = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
   print x
 end
 
